a2a2f782f46bec7c971ffe436675d1cf3e28f1c3
iromli <isman.firmansyah@gmail.com>
2021-05-21 03:19:38 +0700

    fix(spanner): add missing optional_scopes for inserting data

--- scripts/spanner_setup.py
+++ scripts/spanner_setup.py
@@ -235,7 +235,8 @@ class SpannerBackend:
             self.create_spanner_indexes(table_name, column_mapping)
 
     def import_ldif(self):
-        ldif_mappings = get_ldif_mappings()
+        optional_scopes = json.loads(self.manager.config.get("optional_scopes", "[]"))
+        ldif_mappings = get_ldif_mappings(optional_scopes)
 
         ctx = prepare_template_ctx(self.manager)
 
